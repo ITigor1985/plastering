365c4cc0f98c8ca5162275c3b765cdfaebc44b11
Igor <81867285+ITigor1985@users.noreply.github.com>
2021-12-28 17:36:25 +0200

    123

--- src/js/data.js
+++ src/js/data.js
@@ -1,5 +1,17 @@
-export const data = [
+export const dataItems = [
+    
     {
-        price: 200
+        webp1x1440: "./images/price/desktop/img1@1x.webp",
+        webp2x1440: "./images/price/desktop/img1@2x.webp",
+        jpg1x1440: "./images/price/desktop/img1@1x.jpg",
+        jpg2x1440: "./images/price/desktop/img1@2x.jpg",
+        webp1x768: "./images/price/tablet/img1@1x.webp",
+        webp2x768: "./images/price/tablet/img1@2x.webp",
+        jpg1x768:"./images/price/tablet/img1@1x.jpg",
+        jpg2x768: "./images/price/tablet/img1@2x.jpg",
+        webp1xm768: "./images/price/mobile/img1@1x.webp",
+        webp2xm768: "./images/price/mobile/img1@2x.webp",
+        jpg1xm768: "./images/price/mobile/img1@1x.jpg",
+        jpg2xm768: "./images/price/mobile/img1@2x.jpg",
     }
 ]
--- src/js/price.js
+++ src/js/price.js
@@ -1,13 +1,65 @@
-import data from './data';
+import {dataItems} from './data';
 
-const price = document.querySelector('.price__list');
-const galleryImages = galleryItems
-  .map(
-    galleryItem => `
-<a class="gallery__item" href="${galleryItem.original}">
-  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
-</a>
-`,
-  )
-  .join('');
-price.insertAdjacentHTML('beforeend', galleryImages);
+const price = document.querySelector('.services-and-prices__list');
+const prices = dataItems.map(dataItem => `
+<li class="services-and-prices__list-item">
+    <picture>
+        <source
+          media="(min-width: 1440px)"
+          srcset="
+            ${dataItem.webp1x1440} 1x,
+            ${dataItem.webp2x1440} 2x
+          "
+          type="image/webp"
+        />
+        <source
+          media="(min-width: 1440px)"
+          srcset="
+            ${dataItem.jpg1x1440} 1x,
+            ${dataItem.jpg2x1440} 2x
+          "
+        />
+        <source
+          media="(min-width: 768px)"
+          srcset="
+          ${dataItem.webp1x768} 1x,
+          ${dataItem.webp2x768} 2x
+          "
+          type="image/webp"
+        />
+        <source
+          media="(min-width: 768px)"
+          srcset="
+          ${dataItem.jpg1x768} 1x,
+          ${dataItem.jpg2x768} 2x
+          "
+        />
+        <source
+          media="(max-width: 768px)"
+          srcset="
+          ${dataItem.webp1xm768} 1x,
+          ${dataItem.webp2xm768} 2x
+          "
+          type="image/webp"
+        />
+        <source
+          media="(max-width: 768px)"
+          srcset="
+          ${dataItem.jpg1xm768} 1x,
+          ${dataItem.jpg2xm768} 2x
+          "
+        />
+        <img class="services-and-prices__list-item-img" loading="lazy" src="" alt="Антибактериальная обработка внутреннего блока кондиционера " />
+      </picture>
+      <div class="services-and-prices__list-item-wrapper">
+      <p class="services-and-prices__list-item-deskription">
+        Антибактериальная обработка кондиционера
+      </p>
+      <p class="services-and-prices__list-item-cost">ОТ 500 ГРН</p>
+    </div>
+</li>
+`).join('');
+price.insertAdjacentHTML('beforeend', prices);
+
+
+            
