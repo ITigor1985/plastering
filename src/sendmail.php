89fd3bfe7d6a0631daf83f1ab3a82d1b82488261
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-01-05 15:00:13 +0200

    123

--- src/sendmail.php
+++ src/sendmail.php
@@ -6,14 +6,26 @@
     require 'phpmailer/src/PHPMailer.php';
 
     $mail = new PHPMailer(true);
+    $mail ->IsSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Port = 465;
+
+
     $mail->CharSet = 'UTF-8';
     $mail->setLanguage('ru', 'phpmailer/language/');
     $mail->IsHTML(true);
 
+    $mail->Username ="xromer85@gmail.com";
+    $mail->Password = "Fyj34ju27@jt";
     $mail->setFrom('xromer85@gmail.com', 'Привет');
     $mail->addAddress('xromer85@gmail.com');
     $mail->Subject = 'Новый пользователь';
+    $mail->Body = $body;
 
+
+    $body = '<h1>Hello</h1>';
     if(trim(!empty($_POST['name']))){
         $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
     }
