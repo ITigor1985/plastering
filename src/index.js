561d1975fbc30161a6cfd71a72a4809d713085b1
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-01-11 18:32:24 +0200

    123

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'material-icons/iconfont/filled.css';
 import './sass/main.scss';
 
 // import './js/script';
+import './js/reviews';
 import './js/backdrop';
 import './js/video';
 import './js/slick';
---
+++ src/js/reviews.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    $('.reviews__list').slick({         
+      
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      
+      speed: 1000,
+      easing: 'ease', 
+      
+    });
+  
+});
