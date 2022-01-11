83f5f25d7ba11a6487bdbe95ef65edc5cfe1efed
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-01-11 20:43:34 +0200

    123

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -9,5 +9,33 @@ $(document).ready(function () {
       easing: 'ease', 
       
     });
-  
+    
 });
+
+const descriptions = document.querySelectorAll('.reviews__review');
+const windowWidth = document.body.clientWidth;
+    descriptions.forEach(description => {
+      // let styleDescription = window.getComputedStyle(description, null).getPropertyValue("height");
+      let styleDescription = description.clientHeight;
+      console.log(windowWidth);
+      console.log(styleDescription);
+      console.log(description)
+      if((windowWidth <= 767 && parseInt(styleDescription) >= 173)) {
+        description.style.height="172px";
+        description.style.overflow="auto";
+      }      
+      if((windowWidth >= 768 && parseInt(styleDescription) >= 126)) {
+        description.style.height="210px";
+        description.style.overflow="auto";
+      }
+    })
+    
+    
+
+    // let styleDescription = window.getComputedStyle(descriptions, null).getPropertyValue("height");
+    
+
+    // if(windowWidth < 768 && description.style.height > 172){
+    //   description.style.height="172px"
+    //   description.style.overflow="auto";
+    // }
