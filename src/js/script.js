94857978f6c5a2f6ae2dcf17c0db9a7d299d307c
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-02-03 10:17:36 +0200

    123

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,52 @@
+const form = document.querySelector('#form');
 
-    const form = document.querySelector('#form');
-    console.log(form);
-    form.addEventListener('submit', formSend);
+form.addEventListener('submit', formSend);
 
-    async function formSend(e) {
-        e.preventDefault();
+async function formSend(e) {
+  e.preventDefault();
 
-        let error = formValidate(form);
+  let error = formValidate(form);
 
-        let formData = new FormData(form);
+  let formData = new FormData(form);
 
-
-        if(error === 0){
-            let response = await fetch('sendmail.php', {
-                method: 'POST',
-                body: formData
-            });
-            if(response.ok){
-                let result = await response.json();
-                alert(result.message);                
-                form.reset();
-            }else{
-                alert("Ошибка");
-            }
-        }else{
-          alert("Заполните обязательные поля");
-        }
-
+  if (error === 0) {
+    let response = await fetch('sendmail.php', {
+      method: 'POST',
+      body: formData,
+    });
+    if (response.ok) {
+      let result = await response.json();
+      alert(result.message);
+      form.reset();
+    } else {
+      alert('Ошибка');
     }
+  } else {
+    alert('Заполните обязательные поля');
+  }
+}
 
-    function formValidate(form){
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
+function formValidate(form) {
+  let error = 0;
+  let formReq = document.querySelectorAll('._req');
 
-        for(let index = 0; index < formReq.length; index++){
-            const input = formReq[index];
-            formRemoveError(input);
+  for (let index = 0; index < formReq.length; index++) {
+    const input = formReq[index];
+    formRemoveError(input);
 
-            if(input.value === ''){
-                formAddError(input);
-                error++;
-            }
-
-        }
-        return error;
+    if (input.value === '') {
+      formAddError(input);
+      error++;
     }
+  }
+  return error;
+}
 
-    function formAddError(input){
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-    function formRemoveError(input){
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
+function formAddError(input) {
+  input.parentElement.classList.add('_error');
+  input.classList.add('_error');
+}
+function formRemoveError(input) {
+  input.parentElement.classList.remove('_error');
+  input.classList.remove('_error');
+}
